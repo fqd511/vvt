669360385df6609a55ab00ec37b9235801056ef8
511 <fqd511@gmail.com>
2022-03-11 22:40:37 +0800

    chore:update i18n config

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import App from "./App.vue";
 import "normalize.css";
-import { setupLayouts } from 'virtual:generated-layouts'
-import generatedRoutes from 'virtual:generated-pages'
+import { setupLayouts } from "virtual:generated-layouts";
+import generatedRoutes from "virtual:generated-pages";
+import messages from "@intlify/vite-plugin-vue-i18n/messages";
+import { createI18n } from "vue-i18n";
 
-const head = createHead()
-const routes = setupLayouts(generatedRoutes)
+const head = createHead();
+const routes = setupLayouts(generatedRoutes);
+const i18n = createI18n({
+  locale: "en",
+  messages,
+});
 
 createApp(App)
   .use(head)
+  .use(i18n)
   .use(createRouter({ routes, history: createWebHistory() }))
   .mount("#app");
